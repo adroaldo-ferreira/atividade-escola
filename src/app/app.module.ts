22460354b4d6a5a407428175cfb1dad79a68de9e
Adroaldo Ferreira <adroaldo.ferreira@escola.pr.gov.br>
2025-04-30 11:07:35 -0300

    Implementado o sorteio do tapete de porcentagens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TapetePorcentagensComponent } from './tapete-porcentagens/tapete-porcentagens.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TapetePorcentagensComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tapete-porcentagens/tapete-porcentagens.component.ts
@@ -0,0 +1,32 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-tapete-porcentagens',
+  templateUrl: './tapete-porcentagens.component.html',
+  styleUrls: ['./tapete-porcentagens.component.css']
+})
+export class TapetePorcentagensComponent {
+
+  title = 'atividade-escola';
+  valores = [160, 240, 100, 80, 200, 300];
+  percentuais = [10, 15, 50, 100, 25, 75];
+  valor = 0
+  percentual = 0
+
+  resultados = [20, 25, 40, 50, 60, 75, 8, 10, 16, 24, 30, 12, 15, 35, 45, 80, 150, 120, 225, 100, 160, 200, 240, 300, 180];
+
+  sortearValores() {
+    this.valor = this.sortearValor(this.valores);
+    this.percentual = this.sortearValor(this.percentuais);
+  }
+
+  sortearValor(array: number[]): number {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+}
+
+/*type Competidor = {
+  nome: string;
+  cor: string;
+  pontuacao: number;
+}*/
