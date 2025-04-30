a649dd39701c68c8c1fa18aabe507f690c5e3ed3
Adroaldo Ferreira <adroaldo.ferreira@escola.pr.gov.br>
2025-04-30 11:17:58 -0300

    Calculado os valores das respostas

--- src/app/tapete-porcentagens/tapete-porcentagens.component.ts
+++ src/app/tapete-porcentagens/tapete-porcentagens.component.ts
@@ -7,17 +7,20 @@ import {Component} from '@angular/core';
 })
 export class TapetePorcentagensComponent {
 
-  title = 'atividade-escola';
   valores = [160, 240, 100, 80, 200, 300];
   percentuais = [10, 15, 50, 100, 25, 75];
-  valor = 0
-  percentual = 0
+  valor = 0;
+  percentual = 0;
+  respostas: number[] = [];
 
   resultados = [20, 25, 40, 50, 60, 75, 8, 10, 16, 24, 30, 12, 15, 35, 45, 80, 150, 120, 225, 100, 160, 200, 240, 300, 180];
 
   sortearValores() {
     this.valor = this.sortearValor(this.valores);
     this.percentual = this.sortearValor(this.percentuais);
+    const resposta = this.valor * this.percentual / 100.0;
+
+    this.respostas.push(resposta);
   }
 
   sortearValor(array: number[]): number {
