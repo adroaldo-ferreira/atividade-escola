c095fada89f5d63c131773201f23517e8a1a9596
Adroaldo <hygino@msn.com>
2025-05-07 13:58:37 -0300

    Adicionado prime NG, personalizado a navbar e tapete-porcentagens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TapetePorcentagensComponent } from './tapete-porcentagens/tapete-porcentagens.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import { FooterComponent } from "./components/footer/footer.component";
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
-import {FooterComponent} from "./components/footer/footer.component";
+import { TapetePorcentagensComponent } from './tapete-porcentagens/tapete-porcentagens.component';
+
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import {FooterComponent} from "./components/footer/footer.component";
     imports: [
         BrowserModule,
         AppRoutingModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        ButtonModule
     ],
   providers: [],
   bootstrap: [AppComponent]
